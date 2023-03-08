a5598a5efd3931b05fcd2dc004f4081c8d8d3422
SANGWOO PARK <cyberprophet@hanmail.net>
2023-03-09 02:01:11 +0900

    26. rules of firebase

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:yummy_chat/config/palette.dart';
 import 'package:yummy_chat/widgets/login_form_widget.dart';
 import 'package:yummy_chat/widgets/submit_btn_widget.dart';
@@ -15,122 +17,131 @@ class LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Palette.backgroundColor,
-      body: GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
-        child: Stack(
-          children: [
-            Positioned(
-              top: 0,
-              right: 0,
-              left: 0,
-              child: Container(
-                height: 300,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                      'assets/images/red.jpg',
-                    ),
-                    fit: BoxFit.fill,
-                  ),
-                ),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        progressIndicator: const CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation(
+            Palette.googleColor,
+          ),
+        ),
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Stack(
+            children: [
+              Positioned(
+                top: 0,
+                right: 0,
+                left: 0,
                 child: Container(
-                  padding: const EdgeInsets.only(
-                    top: 90,
-                    left: 20,
+                  height: 300,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'assets/images/red.jpg',
+                      ),
+                      fit: BoxFit.fill,
+                    ),
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      RichText(
-                        text: TextSpan(
-                          text: 'Welcome',
+                  child: Container(
+                    padding: const EdgeInsets.only(
+                      top: 90,
+                      left: 20,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text: 'Welcome',
+                            style: const TextStyle(
+                              letterSpacing: 1,
+                              fontSize: 25,
+                              color: Colors.white,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: isSignupScreen
+                                    ? ' to Yummy chat!'
+                                    : ' back',
+                                style: const TextStyle(
+                                  letterSpacing: 1,
+                                  fontSize: 25,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Sign${isSignupScreen ? "up" : "in"} to continue',
                           style: const TextStyle(
                             letterSpacing: 1,
-                            fontSize: 25,
                             color: Colors.white,
                           ),
-                          children: [
-                            TextSpan(
-                              text:
-                                  isSignupScreen ? ' to Yummy chat!' : ' back',
-                              style: const TextStyle(
-                                letterSpacing: 1,
-                                fontSize: 25,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
                         ),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Sign${isSignupScreen ? "up" : "in"} to continue',
-                        style: const TextStyle(
-                          letterSpacing: 1,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            const AnimatedPositioned(
-              duration: Duration(
-                milliseconds: 500,
+              const AnimatedPositioned(
+                duration: Duration(
+                  milliseconds: 500,
+                ),
+                curve: Curves.easeIn,
+                top: 180,
+                child: LoginForm(),
               ),
-              curve: Curves.easeIn,
-              top: 180,
-              child: LoginForm(),
-            ),
-            AnimatedPositioned(
-              duration: const Duration(
-                milliseconds: 500,
+              AnimatedPositioned(
+                duration: const Duration(
+                  milliseconds: 500,
+                ),
+                curve: Curves.easeIn,
+                top: isSignupScreen ? 430 : 390,
+                right: 0,
+                left: 0,
+                child: const SubmitButton(),
               ),
-              curve: Curves.easeIn,
-              top: isSignupScreen ? 430 : 390,
-              right: 0,
-              left: 0,
-              child: const SubmitButton(),
-            ),
-            AnimatedPositioned(
-              duration: const Duration(
-                milliseconds: 500,
-              ),
-              curve: Curves.easeIn,
-              top: MediaQuery.of(context).size.height -
-                  (isSignupScreen ? 125 : 165),
-              right: 0,
-              left: 0,
-              child: Column(
-                children: [
-                  Text(
-                    'or Sign${isSignupScreen ? "up" : "in"} with',
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      backgroundColor: Colors.transparent,
-                      maximumSize: const Size(
-                        180,
-                        48,
+              AnimatedPositioned(
+                duration: const Duration(
+                  milliseconds: 500,
+                ),
+                curve: Curves.easeIn,
+                top: MediaQuery.of(context).size.height -
+                    (isSignupScreen ? 125 : 165),
+                right: 0,
+                left: 0,
+                child: Column(
+                  children: [
+                    Text(
+                      'or Sign${isSignupScreen ? "up" : "in"} with',
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                        backgroundColor: Colors.transparent,
+                        maximumSize: const Size(
+                          180,
+                          48,
+                        ),
+                      ),
+                      child: Image.asset(
+                        'assets/images/btn_google_signin_light_normal.png',
                       ),
                     ),
-                    child: Image.asset(
-                      'assets/images/btn_google_signin_light_normal.png',
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -144,7 +155,9 @@ class LoginScreenState extends State<LoginScreen> {
     }
   }
 
+  final authentication = FirebaseAuth.instance;
   final formKey = GlobalKey<FormState>();
   String userName = '', userEmail = '', userPassword = '';
   bool isSignupScreen = false;
+  bool showSpinner = false;
 }
--- lib/widgets/submit_btn_widget.dart
+++ lib/widgets/submit_btn_widget.dart
@@ -1,9 +1,19 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:yummy_chat/screens/chat_screen.dart';
 import 'package:yummy_chat/screens/login_screen.dart';
 
-class SubmitButton extends StatelessWidget {
+class SubmitButton extends StatefulWidget {
   const SubmitButton({super.key});
 
+  @override
+  State<SubmitButton> createState() => _SubmitButtonState();
+}
+
+class _SubmitButtonState extends State<SubmitButton> {
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -18,9 +28,48 @@ class SubmitButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(50),
         ),
         child: GestureDetector(
-          onTap: () => context
-              .findAncestorStateOfType<LoginScreenState>()!
-              .tryValidation(),
+          onTap: () async {
+            final ls = context.findAncestorStateOfType<LoginScreenState>()!;
+            ls.setState(() => ls.showSpinner = true);
+            ls.tryValidation();
+
+            try {
+              final bytes = utf8.encode(ls.userPassword);
+              final digest = sha512.convert(bytes);
+              UserCredential sub;
+
+              if (ls.isSignupScreen) {
+                sub = await ls.authentication.createUserWithEmailAndPassword(
+                  email: ls.userEmail,
+                  password: digest.toString(),
+                );
+              } else {
+                sub = await ls.authentication.signInWithEmailAndPassword(
+                  email: ls.userEmail,
+                  password: digest.toString(),
+                );
+              }
+              if (context.mounted) {
+                return;
+              }
+              if (sub.user != null) {
+                Navigator.push(
+                  ls.context,
+                  MaterialPageRoute(
+                    builder: (_) => const ChatScreen(),
+                  ),
+                );
+              }
+            } catch (e) {
+              ScaffoldMessenger.of(ls.context).showSnackBar(
+                SnackBar(
+                  content: Text('${(e as FirebaseAuthException).message}'),
+                  backgroundColor: Colors.blue,
+                ),
+              );
+            }
+            ls.setState(() => ls.showSpinner = false);
+          },
           child: Container(
             decoration: BoxDecoration(
               gradient: const LinearGradient(
